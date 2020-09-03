f87064382fd18e6defd1c20e2110876d0eb54d5f
zxf@rasp <xfz79@foxmail.com>
2020-09-03 09:28:05 +0800

    add URL

--- demo2.py
+++ demo2.py
@@ -19,7 +19,7 @@ from openpyxl import Workbook
 #SECRET_KEY = 'igeVigIi65Np9Vv0K6yL0U2mTpkyhdsd'
 warnings.filterwarnings("ignore")
 
-with open(r'F:\0testexe\FBIOS\V3.01/ipconfig.txt') as f:
+with open(r'/home/pi/Documents/WinUIAutomation') as f:
 #with open('./ipconfig.txt') as f:
     text = f.read()
     name = text.split('\n')
