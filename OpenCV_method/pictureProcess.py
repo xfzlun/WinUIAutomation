be57d37b74085a8c1e6e2c84850e24d5e992f561
zxf@rasp <xfz79@foxmail.com>
2020-09-09 10:25:15 +0800

    Update Picprocess.py

--- OpenCV_method/pictureProcess.py
+++ OpenCV_method/pictureProcess.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 import matplotlib.pyplot as pyplot
 
-picPath = '~/Documents/result.png'  #先标明目前工作目录
+picPath = '~/Documents/result.png'  #标明图片档案所在位置
 fullPath = os.path.expanduser(picPath)  #
 print(fullPath)
 cap = cv2.imread(fullPath)
